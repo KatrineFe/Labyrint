d71b7ff14e738521860c3d72cfc3045657772978
Nina <niz6@yandex.ru>
2024-01-08 00:49:00 +0300

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- dz/Header1.h
+++ dz/Header1.h
@@ -8,3 +8,6 @@ struct Point {
 
 bool solveMaze(vector<vector<int>>& maze, vector<vector<string>>& new_maze, Point start, Point end);
 bool isValid(int x, int y, vector<vector<int>>& maze);
+
+//:(((((((((((((((((((((((((((((
+//))))))))))))))))))))))))))))):
